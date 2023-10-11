741af026579d0ee9ceb02012afbc5ab806e10a08
Sam Badiei <sambadiei@Sams-Air.home>
2023-10-11 22:05:18 +0300

    get build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,5 @@
-import CredentialsProvider from "next-auth/providers/credentials";
+import { authOptions } from "@/app/authOptions";
 import NextAuth from "next-auth";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/app/firebase/init";
-
-export const authOptions = {
-  pages: {
-    signIn: "/SignIn",
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {},
-      async authorize(credentials, req): Promise<any> {
-        console.log("cred:", credentials);
-
-        return await signInWithEmailAndPassword(
-          auth,
-          (credentials as any).email || "",
-          (credentials as any).password || ""
-        )
-          .then((user) => {
-            //console.log(user);
-
-            if (user) {
-              return user.user;
-            }
-
-            return null;
-          })
-          .catch((e) => {
-            return null;
-          });
-      },
-    }),
-  ],
-};
 
 const handler = NextAuth(authOptions);
 
---
+++ app/authOptions.ts
@@ -0,0 +1,36 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/app/firebase/init";
+import { NextAuthOptions } from "next-auth";
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/SignIn",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials, req): Promise<any> {
+        console.log("cred:", credentials);
+
+        return await signInWithEmailAndPassword(
+          auth,
+          (credentials as any).email || "",
+          (credentials as any).password || ""
+        )
+          .then((user) => {
+            //console.log(user);
+
+            if (user) {
+              return user.user;
+            }
+
+            return null;
+          })
+          .catch((e) => {
+            return null;
+          });
+      },
+    }),
+  ],
+};
